b78f014f2046b6595a99f148c047fff3ede8c808
Victoria Stubbolo <victoriaelizabeths@gmail.com>
2018-04-05 07:30:57 -0700

    checkpoint 8 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -79,9 +79,41 @@ class MenuController
   end
 
   def search_entries
+
+    print "Search by name: "
+    name = gets.chomp
+
+    match = address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
   end
 
   def read_csv
+
+    print "Enter CSV file to import: "
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} new entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+      read_csv
+    end
+  end
 
   def entry_submenu(entry)
 
@@ -97,8 +129,10 @@ class MenuController
     when "n"
 
     when "d"
+      delete_entry(entry)
     when "e"
-
+      edit_entry(entry)
+      entry_submenu(entry)
     when "m"
       system "clear"
       main_menu
@@ -108,5 +142,54 @@ class MenuController
       entry_submenu(entry)
     end
   end
-end
+
+  def delete_entry(entry)
+    address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+
+    print "Updated name: "
+    name = gets.chomp
+    print "Updated phone number"
+    phone_number = gets.chomp
+    print "Updated email: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entrty.phone_number = phone _number if !phone_number.empty?
+    entry.email = email if !email.empty?
+
+    puts "Updated entry: "
+    puts entry
+  end
+
+  def search_submenu(entry)
+
+    puts "\nd - delete entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+
+    selection = gets.chomp
+
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
+  end
 end
