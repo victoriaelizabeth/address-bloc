716f1614d5e8465e89f93becd3d74edab623796b
Victoria Stubbolo <victoriaelizabeths@gmail.com>
2018-06-18 15:29:22 -0700

    Assignment 8 revised

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,7 +9,7 @@ class AddressBook
   end
 
   def nuke
-    
+    @entries = []
   end
 
   def add_entry(name, phone_number, email)
--- spec/address_book.rb
+++ spec/address_book.rb
@@ -10,6 +10,17 @@ RSpec.describe AddressBook do
     expect(entry.email).to eq expected_email
   end
 
+  context "#nuke" do
+    it "should delete all entries" do
+      book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+      book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+      book.add_entry("Ada Lovelace", "010.012.1815", "augusta.king@lovelace.com")
+
+      book.nuke
+      expect(book.entries.size).to eq 0 
+    end
+  end
+
   describe "attributes" do
     it "responds to entries" do
       expect(book).to respond_to(:entries)
